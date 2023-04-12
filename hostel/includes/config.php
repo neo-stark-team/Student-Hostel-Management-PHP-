3ceecba14b74c284b4d8d17497b2955f8f085009
Mathankumar R <rmathankumarcpm@gmail.com>
2023-04-12 17:11:57 +0530

    First commit

---
+++ hostel/includes/config.php
@@ -0,0 +1,7 @@
+<?php
+$dbuser="admin";
+$dbpass="examly2023";
+$host="database-1.c7j0ofgsqkxi.us-east-1.rds.amazonaws.com";
+$db="hostelmanagement";
+$mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+?>
